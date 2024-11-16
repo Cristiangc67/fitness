58a0715ebb4cc82f3124420df0310996d27416d7
gigiar <gigiar54@gmail.com>
2024-11-16 14:06:05 -0300

    planes alimenticios por pagina

--- apps/planAlimentacion/views.py
+++ apps/planAlimentacion/views.py
@@ -6,3 +6,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 class MealPlansView(LoginRequiredMixin, TemplateView):
     template_name = "planAlimentacion/mealPlans.html"
+
+class ProteicaView(LoginRequiredMixin, TemplateView):
+    template_name = "planAlimentacion/proteica.html"
+
+class VeganaView(LoginRequiredMixin, TemplateView):
+    template_name = "planAlimentacion/vegana.html"
+
+class VegetarianaView(LoginRequiredMixin, TemplateView):
+    template_name = "planAlimentacion/vegetariana.html"
