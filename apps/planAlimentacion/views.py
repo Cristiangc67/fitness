c3caac62bae369f3aad2b1fdc143020f55962b70
Cristian <cristiangutierrezcruz67@gmail.com>
2024-11-17 16:46:34 -0300

    autenticacion en planes de alimentacion

--- apps/planAlimentacion/views.py
+++ apps/planAlimentacion/views.py
@@ -7,11 +7,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class MealPlansView(LoginRequiredMixin, TemplateView):
     template_name = "planAlimentacion/mealPlans.html"
 
+    def get_context_data(self, **kwargs: any):
+        context = super().get_context_data(**kwargs)
+        context["is_authenticated"] = self.request.user.is_authenticated
+        return context
+
+
 class ProteicaView(LoginRequiredMixin, TemplateView):
     template_name = "planAlimentacion/proteica.html"
 
+
 class VeganaView(LoginRequiredMixin, TemplateView):
     template_name = "planAlimentacion/vegana.html"
+
 
 class VegetarianaView(LoginRequiredMixin, TemplateView):
     template_name = "planAlimentacion/vegetariana.html"
