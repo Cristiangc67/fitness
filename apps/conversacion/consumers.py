167f53f0ed6b869c0610e70de88cc17a6560d1df
Cristian <cristiangutierrezcruz67@gmail.com>
2024-11-19 14:25:31 -0300

    chat eliminar mensaje

--- apps/conversacion/consumers.py
+++ apps/conversacion/consumers.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.contrib.auth import get_user_model
 from .models import Conversacion, Mensaje
+from django.utils import timezone
+
 
 User = get_user_model()
 
@@ -15,51 +17,64 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name, self.channel_name
         )
         self.accept()
+        print("WebSocket connected")
 
-        self.send(
-            text_data=json.dumps(
-                {"type": "connection_established", "message": "you are now connected!"}
-            )
-        )
-
-    """ def disconnect(self, close_code):
-        # Salir del grupo de la conversación en Redis
+    def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
-        ) """
+        )
+        print("WebSocket disconnected")
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message_content = text_data_json["message"]
-        sender = self.scope["user"]
-        conversation = Conversacion.objects.get(id=self.room_name)
-        message = Mensaje.objects.create(
-            conversation=conversation,
-            message=message_content,
-            sender=sender,
-        )
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": message_content,
-                "sender": sender.username,
-                "id": sender.id,
-                "timestamp": str(message.created),
-            },
-        )
+        data = json.loads(text_data)
+        action = data.get("action")
+        print(f"Received action: {action}")
 
-        """ self.send(
-            text_data=json.dumps(
+        if action == "send_message":
+            message_content = data["message"]
+            sender = self.scope["user"]
+            conversation = Conversacion.objects.get(id=self.room_name)
+            message = Mensaje.objects.create(
+                conversation=conversation,
+                message=message_content,
+                sender=sender,
+            )
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name,
                 {
-                    "type": "chat",
+                    "type": "chat_message",
                     "message": message_content,
                     "sender": sender.username,
-                    "sender_id": sender.id,
+                    "id": sender.id,
                     "timestamp": str(message.created),
-                }
+                    "message_id": message.id,
+                },
             )
-        ) """
+            print(f"Message sent: {message_content}")
+
+        elif action == "delete_message":
+            message_id = data.get("message_id")
+            print(f"Attempting to delete message with ID: {message_id}")
+            try:
+                message = Mensaje.objects.get(id=message_id)
+                print(f"Mensaje encontrado: {message}")
+                print(f"Mensaje encontrado: {message.message}")
+                message.delete = timezone.now()
+                message.save()
+
+                if message.sender == self.scope["user"]:
+                    message.delete = timezone
+                    async_to_sync(self.channel_layer.group_send)(
+                        self.room_group_name,
+                        {
+                            "type": "delete_message",
+                            "message_id": message_id,
+                            "deleted_at": str(message.delete),
+                        },
+                    )
+                    print(f"Message deleted: {message_id}")
+            except Mensaje.DoesNotExist:
+                print("Message does not exist")
 
     def chat_message(self, event):
         self.send(
@@ -70,6 +85,17 @@ class ChatConsumer(WebsocketConsumer):
                     "sender": event["sender"],
                     "sender_id": event["id"],
                     "timestamp": event["timestamp"],
+                    "message_id": event["message_id"],
+                }
+            )
+        )
+
+    def delete_message(self, event):
+        self.send(
+            text_data=json.dumps(
+                {
+                    "type": "delete_message",
+                    "message_id": event["message_id"],
                 }
             )
         )
--- static/js/chat.js
+++ static/js/chat.js
@@ -4,16 +4,28 @@ const messagesContainer = document.getElementById("messages");
 scrollToBottom();
 let url = `ws://${window.location.host}/ws/chat/${roomName}/`;
 const chatSocket = new WebSocket(url);
+
 function scrollToBottom() {
   messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
+chatSocket.onopen = function () {
+  console.log("WebSocket connection opened");
+};
+
+chatSocket.onclose = function () {
+  console.log("WebSocket connection closed");
+};
+
 chatSocket.onmessage = function (e) {
   let data = JSON.parse(e.data);
-
   let userId = parseInt(user_id);
 
   if (data.type === "chat") {
+    if (data.deleted_at) {
+      console.log("Mensaje eliminado, no mostrarlo.");
+      return; // No mostrar el mensaje
+    }
     let messages = document.getElementById("messages");
 
     if (data.sender_id === userId) {
@@ -35,27 +47,62 @@ chatSocket.onmessage = function (e) {
     const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
       date
     );
+    console.log(data.id);
 
     messages.insertAdjacentHTML(
       "beforeend",
-      `<div class="message-container ${side}">
+      `<div class="message-container ${side}" data-message-id="${
+        data.message_id
+      }">
         <div class="message ${messageStyle}">
           <strong>${data.sender}</strong>
           <p>${data.message}</p>
           (${formattedDate})
+          ${
+            data.sender_id === userId
+              ? `<button class="delete-message btn btn-danger" data-message-id="${data.message_id}">Eliminar</button>`
+              : ""
+          }
         </div>
       </div>`
     );
     scrollToBottom();
+  } else if (data.type === "delete_message") {
+    console.log(`Message ID to delete: ${data.message_id}`);
+    const messageElement = document.querySelector(
+      `.message-container[data-message-id="${data.message_id}"]`
+    );
+    console.log(messageElement);
+    if (messageElement) {
+      messageElement.remove();
+    }
   }
 };
 
+// Manejar la eliminación de mensajes
+document.addEventListener("click", function (event) {
+  if (event.target && event.target.classList.contains("delete-message")) {
+    const messageId = event.target.getAttribute("data-message-id");
+    console.log(`Message ID to delete: ${messageId}`);
+
+    chatSocket.send(
+      JSON.stringify({
+        action: "delete_message",
+        message_id: messageId,
+      })
+    );
+  }
+});
+
+// Enviar mensaje cuando se envía el formulario
 let form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let message = e.target.message.value;
+
   chatSocket.send(
     JSON.stringify({
+      action: "send_message", // Añade explícitamente el campo 'action'
       message: message,
     })
   );
